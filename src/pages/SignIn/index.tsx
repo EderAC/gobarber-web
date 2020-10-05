a424afe79e0368462010ebfadec6505ed66bc07d
EderAC <eder.almeida40@gmail.com>
2020-10-05 18:27:02 -0300

    added function to add toast when needed

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -55,7 +55,11 @@ const SignIn: React.FC = () => {
         }
 
         // dispara um toast
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
+        });
       }
     },
     [signIn, addToast],
